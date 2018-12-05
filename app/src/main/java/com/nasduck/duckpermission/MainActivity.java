a68c9666eb5eba3165fe6776ba847970716479bb
Chuan DONG <dongchuanyz@163.com>
2018-12-05 17:17:11 +0800

    Add demo code for audio record

--- app/src/main/java/com/nasduck/duckpermission/MainActivity.java
+++ app/src/main/java/com/nasduck/duckpermission/MainActivity.java
@@ -1,13 +1,42 @@
 package com.nasduck.duckpermission;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static int AUDIO_RECORD_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case AUDIO_RECORD_CODE:
+                if (DuckPermission.getInstance(this).result(grantResults)) {
+                    Toast.makeText(this, "Granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    public void onAudioRecordClick(View view) {
+        if (DuckPermission.getInstance(this)
+                .setResultCode(AUDIO_RECORD_CODE)
+                .requestAudioRecord()
+                .request()) {
+            Toast.makeText(this, "Already granted audio record permission", Toast.LENGTH_SHORT).show();
+        }
     }
 }
