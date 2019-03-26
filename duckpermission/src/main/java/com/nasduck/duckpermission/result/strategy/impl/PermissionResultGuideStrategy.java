3275568ae9459e2d623df9bdd2e53e5b903262b4
Chuan DONG <dongchuanyz@163.com>
2019-03-26 10:13:35 +0800

    Update guide strategy

--- duckpermission/src/main/java/com/nasduck/duckpermission/result/strategy/impl/PermissionResultGuideStrategy.java
+++ duckpermission/src/main/java/com/nasduck/duckpermission/result/strategy/impl/PermissionResultGuideStrategy.java
@@ -1,15 +1,10 @@
 package com.nasduck.duckpermission.result.strategy.impl;
 
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 
 import com.nasduck.duckpermission.R;
-import com.nasduck.duckpermission.result.listener.OnPermissionResultListener;
+import com.nasduck.duckpermission.result.dialog.DialogUtils;
 import com.nasduck.duckpermission.result.strategy.IPermissionResultStrategy;
 import com.nasduck.duckpermission.util.PermissionUtils;
 
@@ -27,7 +22,7 @@ public class PermissionResultGuideStrategy implements IPermissionResultStrategy 
 
         if (deniedPermissions.size() != 0) {
             String name = PermissionUtils.translatePermissions(context, deniedPermissions);
-            showGuideDialog(context, requestCode, name);
+            showGuideDialog(context, name);
         }
     }
 
@@ -37,32 +32,13 @@ public class PermissionResultGuideStrategy implements IPermissionResultStrategy 
      * @param context
      * @param permissions
      */
-    private void showGuideDialog(final Context context, final int requestCode, String permissions) {
+    private void showGuideDialog(final Context context, String permissions) {
         StringBuilder sb = new StringBuilder();
         sb.append(context.getString(R.string.guide_title) + "\n\n");
         sb.append(permissions);
         sb.append("\n" + context.getString(R.string.guide_footer));
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setMessage(sb.toString());
-        builder.setPositiveButton(context.getString(R.string.grant), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent i = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                        Uri.parse("package:" + context.getPackageName()));
-                i.addCategory(Intent.CATEGORY_DEFAULT);
-                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-                context.startActivity(i);
-            }
-        });
-        builder.setNegativeButton(context.getString(R.string.cancel), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        builder.show();
+        // Show Guide Dialog
+        DialogUtils.showGuideDialog(context, sb.toString());
     }
 }
